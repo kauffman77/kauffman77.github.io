c7eeae7a4491c0e7a40a01ad852ba4d6f551c4b0
Chris Kauffman <kauffman77@gmail.com>
2023-05-03 10:10:36 -0500

    Push Update

--- 2021/gradecalc.js
+++ 2021/gradecalc.js
@@ -18,7 +18,7 @@ var model = [
     // Assignment("Exam 3", 60, 15, false),
     Assignment("Final Exam", 70, 20, false),
     Assignment("Total of Top 12 HW Quizzes", 120, 10, false),
-    Assignment("Labs + Surveys", 13, 0, true),
+    Assignment("Labs + Surveys", 14, 0, true),
     Assignment("Bonus Engage", 0, 0, true),
     Assignment("Project Late Days", 0, 0, true),
     Assignment("<i>Computed Engagement Points</i>", 10, 10, "NoInput"),
