943f7abb8145f4ab07c6f03d36d5f2b447346751
Chris Kauffman <kauffman77@gmail.com>
2024-09-11 00:06:31 -0400

    Push Update

---
+++ 216/p1-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo No updates at this time
+# echo Making a safety zip
+# make zip > /dev/null
+# mv --backup=numbered p4-complete.zip ../safety-p4-complete.zip
+
+# echo Moving old file versions to .backup/
+# mkdir -p .backup
+# mv --backup=numbered Makefile test_* testy data data34 data5 bake_util.c .backup
+
+# echo Downloading new files
+# rm -f p4-update.zip
+# wget https://www.cs.umd.edu/~profk/216/p4-update.zip
+# unzip -qq p4-update.zip
+# rm -f p4-update.zip
+
+# echo Project Updated
