dd6ed41268b5ccee23ae50941579f96605ac53ab
Chris Kauffman <kauffman77@gmail.com>
2024-11-11 14:25:13 -0500

    Push Update

---
+++ 216/update_lab10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+url=https://www.cs.umd.edu/~profk/216/upload
+
+rm -f Makefile test_newer_file.org test_redirect_child.org
+wget $url/Makefile
+wget $url/test_newer_file.org
+wget $url/test_redirect_child.org
+
+echo LAB UPDATED
