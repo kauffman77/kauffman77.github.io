6d527cf26d73d858a94273e725ef1c2e56707d00
Chris Kauffman <kauffman77@gmail.com>
2024-12-01 17:34:01 -0500

    Push Update

---
+++ 216/p5-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'No updates at this time'
+# echo 'Creating a backup zip in ../p5-complete-backup.zip'
+# make zip > /dev/null
+# mv --backup=numbered p5-complete.zip ../p5-complete-backup.zip
+
+# echo 'Retrieving zip of updated provided files'
+# # rm -rf data/   # special case to remove the crummy data directory
+# wget -q https://www.cs.umd.edu/~profk/216/p5-update.zip
+
+# echo 'Unpacking updated versions of provided files'
+# unzip -qq -o p5-update.zip
+# rm -f p5-update.zip
+
+# echo 'Project Update Complete'
