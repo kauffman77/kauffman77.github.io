1ebc467ce932fec6569fb30bee7a1a05c6a3fb32
Chris Kauffman <kauffman77@gmail.com>
2025-03-24 09:52:43 -0400

    Push Update

--- 216/p3-update.sh
+++ 216/p3-update.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo 'No updates at this time'
-# echo 'Creating a backup zip in ../p2-complete-backup.zip'
-# make zip > /dev/null
-# mv --backup=numbered p2-complete.zip ../p2-complete-backup.zip
+# echo 'No updates at this time'
+echo 'Creating a backup zip in ../p3-complete-backup.zip'
+make zip > /dev/null
+mv --backup=numbered p2-complete.zip ../p3-complete-backup.zip
 
-# echo 'Retrieving zip of updated provided files'
-# # rm -rf data/   # special case to remove the crummy data directory
-# wget -q https://www.cs.umd.edu/~profk/216/p2-update.zip
+echo 'Retrieving zip of updated provided files'
+wget -q https://www.cs.umd.edu/~profk/216/p3-update.zip
 
-# echo 'Unpacking updated versions of provided files'
-# unzip -qq -o p2-update.zip
-# rm -f p2-update.zip
+echo 'Unpacking updated versions of provided files'
+unzip -qq -o p3-update.zip
+rm -f p3-update.zip
 
-# echo 'Project Update Complete'
+echo 'Project Update Complete'
