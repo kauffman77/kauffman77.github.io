d5f030db978be6f590ea91c75f7b127172b418a4
Chris Kauffman <kauffman77@gmail.com>
2025-08-27 17:30:56 -0400

    Push Update

--- 216/lab10-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-url=https://www.cs.umd.edu/~profk/216/lab10-update.zip
-
-rm -f Makefile test_newer_file.org test_redirect_child.org
-wget $url
-unzip lab10-update.zip
-rm -f lab10-update.zip
-echo LAB UPDATED
