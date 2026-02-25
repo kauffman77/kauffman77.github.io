1b1329e1233593dac1f41c60bced62bc86ceb4b7
Chris Kauffman <kauffman77@gmail.com>
2026-02-25 11:25:44 -0500

    Push Update

---
+++ 416/a2-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+AN = a2
+CLASS = 416
+
+# echo 'No updates at this time'
+echo 'Creating a backup zip in ../${AN}-complete-backup.zip'
+make zip > /dev/null
+mv --backup=numbered ${AN}-complete.zip ../${AN}-complete-backup.zip
+
+echo 'Retrieving zip of updated provided files'
+# rm -rf data/   # special case to remove the crummy data directory
+wget -q https://www.cs.umd.edu/~profk/${CLASS}/${AN}-update.zip
+
+echo 'Unpacking updated versions of provided files'
+unzip -qq -o ${AN}-update.zip
+rm -f ${AN}-update.zip
+
+echo 'Update Complete'
