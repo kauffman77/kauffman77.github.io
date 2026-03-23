532ce1beebddcbc27c0bf75030b9eeef5dc22921
Chris Kauffman <kauffman77@gmail.com>
2026-03-23 10:08:57 -0400

    Push Update

--- 216/p3-update.sh
+++ 216/p3-update.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo 'No updates at this time'
-# echo 'Creating a backup zip in ../p3-complete-backup.zip'
-# make zip > /dev/null
-# mv --backup=numbered p3-complete.zip ../p3-complete-backup.zip
+# echo 'No updates at this time'
 
-# echo 'Retrieving zip of updated provided files'
-# wget -q https://www.cs.umd.edu/~profk/216/p3-update.zip
+echo 'Creating a backup zip in ../p3-complete-backup.zip'
+make zip > /dev/null
+mv --backup=numbered p3-complete.zip ../p3-complete-backup.zip
 
-# echo 'Unpacking updated versions of provided files'
-# unzip -qq -o p3-update.zip
-# rm -f p3-update.zip
+echo 'Retrieving zip of updated provided files'
+wget -q https://www.cs.umd.edu/~profk/216/p3-update.zip
 
-# echo 'Project Update Complete'
+echo 'Unpacking updated versions of provided files'
+unzip -qq -o p3-update.zip
+rm -f p3-update.zip
+
+echo 'Project Update Complete'
